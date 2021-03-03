e3585302878b8137a21fe30eeedfc3fecb32fe13
brettjrea <6890798+brettjrea@users.noreply.github.com>
2021-03-02 22:47:23 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ apt update -y && apt dist-upgrade -y
 apt install update-manager-core -y
 ###Configure available releases to LTS.
 sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
-###Switch repos from bionic to disco.
+### Change from Buster to Bullseye.
 sed -i 's/buster/bullseye/g' /etc/apt/sources.list
 sed -i 's/^/#/' /etc/apt/sources.list.d/*.list
 apt update -y
