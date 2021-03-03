ac3a4bcdc5f654d6d3490ea36dae3ed225f2d664
brettjrea <6890798+brettjrea@users.noreply.github.com>
2021-03-02 22:56:51 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
+### Disable Prompts.
+DEBIAN_FRONTEND=noninteractive
 ###Update & Upgrade.
 apt update -y && apt dist-upgrade -y
-###Install update manager core.
-apt install update-manager-core -y
 ###Configure available releases to LTS.
 sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
 ###Change from Buster to Bullseye.
 sed -i 's/buster/bullseye/g' /etc/apt/sources.list
-sed -i 's/^/#/' /etc/apt/sources.list.d/*.list
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
