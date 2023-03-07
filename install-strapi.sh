7777b66892516b51c0817c6d263094762dbd1ee7
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-03-06 21:37:16 -0500

    Update install-strapi.sh

--- install-strapi.sh
+++ install-strapi.sh
@@ -19,7 +19,7 @@ yarn global add pm2
 yarn create strapi-app my-backend --quickstart --no-run
 
 # Navigate to the newly created Strapi project directory
-cd my-backend
+cd my-backend && echo "18.14.2" > .nvmrc
 
 # Install the init admin user plugin
 yarn add strapi-plugin-init-admin-user
